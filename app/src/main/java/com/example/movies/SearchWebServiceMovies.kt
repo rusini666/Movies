bc6f099788358dd45ea6cc65df0d0fc2a5fdd875
Rusini Thara <72293107+rusini666@users.noreply.github.com>
2022-04-16 22:31:53 +0530

    Added repository

---
+++ app/src/main/java/com/example/movies/SearchWebServiceMovies.kt
@@ -0,0 +1,11 @@
+package com.example.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchWebServiceMovies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_web_service_movies)
+    }
+}
